b20d39c727aa11cff6f8fae52d020dc3ca5772ff
nezinaugoblinas5@gmail.com <nezinaugoblinas5@gmail.com>
2018-06-26 16:32:38 +0300

    added user model, added isempty function for validation, added validation, added body parser, now registration is fully working

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Creating Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+// TODO: ADD MORE information to user model
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-// @riyte   GET api/users/test
-// @desc    Tests users route
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
+// Load User model
+const User = require('../../models/User');
+
+// @riyte   GET api/users/register
+// @desc    Register
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: "Users works"})
-);
+router.post('/register', (req, res) => {
+  const {errors, isValid } = validateRegisterInput(req.body);
+
+  // Check for validity
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email })
+  .then(user => {
+    if(user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash)=> {
+          if(err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+
 
 module.exports = router;
