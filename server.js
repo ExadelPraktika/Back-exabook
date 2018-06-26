603ce516af8b26c4ea93ae7b7636fc2c9642f5d2
nezinaugoblinas5@gmail.com <nezinaugoblinas5@gmail.com>
2018-06-26 15:33:05 +0300

    added gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+
 const app = express();
 
 // DB Config
@@ -13,6 +15,9 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello'));
+
+// Routes
+app.use('/api/users', users);
 
 const port = 5000;
 
